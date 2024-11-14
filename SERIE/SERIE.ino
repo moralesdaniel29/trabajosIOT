140d01e48c74163bc1ba4067da55096aa97d535c
moralesdaniel29 <ironigevening@gmail.com>
2024-11-14 08:39:22 -0600

    Add files via upload
    En esta practica se realizo una serie de 7 foquitos, que deben prender en sucesión uno después del otro.

---
+++ SERIE/SERIE.ino
@@ -0,0 +1,81 @@
+const int led1 = 13;
+const int led2 = 12;
+const int led3 = 11;
+const int led4 = 10;
+const int led5 = 9;
+const int led6 = 8;
+const int led7 = 7; 
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);  
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  delay(500);
+
+ digitalWrite(led3, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  delay(500); 
+
+  digitalWrite(led3, HIGH);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  delay(500); 
+
+  digitalWrite(led3, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  delay(500); 
+
+  digitalWrite(led3, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, LOW);
+  delay(500); 
+  
+  digitalWrite(led3, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, LOW);
+  delay(500); 
+
+  digitalWrite(led3, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  digitalWrite(led7, HIGH);
+  delay(500);
+}
