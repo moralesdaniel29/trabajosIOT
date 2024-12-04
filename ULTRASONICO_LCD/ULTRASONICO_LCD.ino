49a799e7d98009d0f4722d3bfcab2152f5d93364
moralesdaniel29 <ironigevening@gmail.com>
2024-12-04 16:42:52 -0600

    Add files via upload
    En esta práctica utilizamos la pantalla LCD y el sensor ultrasónico. Estos componentes se emplearon para que el sensor detectara la distancia de un objeto y la pantalla LCD mostrara la distancia en centímetros.

---
+++ ULTRASONICO_LCD/ULTRASONICO_LCD.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd (19,18,17,16,15,14);
+
+int trig = 10;
+int eco = 9;
+int led = 8;
+
+int duracion;
+int distancia;
+
+void setup(){
+  pinMode(trig, OUTPUT);
+  pinMode(eco, INPUT);
+  pinMode(led, OUTPUT);
+  //Serial.begin(9600);
+  lcd.begin(16,2);
+  }
+
+
+void loop (){
+  digitalWrite(trig, HIGH);
+  delay(1);
+  digitalWrite(trig, LOW);
+  duracion = pulseIn(eco, HIGH);
+  distancia = duracion / 58.2;  
+    lcd.setCursor(1 ,0);
+    lcd.print(distancia);
+    lcd.print(" cm");
+  delay(200);
+if(distancia >= 30){
+    digitalWrite(led, LOW);
+    delay(50);
+  
+} else {  
+    digitalWrite(led, HIGH);
+    delay(50);
+}
+
+}
+
+
