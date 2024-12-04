f6c78a042bd71e8c29199979e9701dac3e3c7dba
moralesdaniel29 <ironigevening@gmail.com>
2024-12-04 17:03:50 -0600

    Add files via upload
    Este componente nos ayudó a hacer que un solo LED mostrara diferentes colores sin la necesidad de utilizar múltiples LEDs individuales.

---
+++ LED_RGB/led_rgb.ino
@@ -0,0 +1,31 @@
+int rojo= 10;
+int verde=9;
+int azul=8;
+
+void setup(){
+  pinMode(rojo, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(azul, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  //blanco
+  /*analogWrite(rojo, 255);
+  analogWrite(verde, 255);
+  analogWrite(azul, 255);*/
+  //amarillo
+  /*analogWrite(rojo, 255);
+  analogWrite(verde, 255);
+  analogWrite(azul, 0);*/
+  //rosa 
+  /*analogWrite(rojo, 255);
+  analogWrite(verde, 0);
+  analogWrite(azul, 255);*/
+  //cafe
+  analogWrite(rojo, 63);
+  analogWrite(verde, 34);
+  analogWrite(azul, 19);
+}
+
